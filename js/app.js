f297dafd932de344f37e0ccb5f9e8132a4ef224f
christianbondoc <christianraebondoc@hotmail.com>
2017-10-25 14:01:30 -0700

    Cleaned click functions, added for loops to get monster Stats, each monster stat reflects card calculations (so far)

--- js/app.js
+++ js/app.js
@@ -36,139 +36,63 @@ var LCommander = document.getElementById("lightCommander"),
 
 
 
-/* 
-    
-   ___  ___    _ ___ ___ _____ ___ _ 
+/* ___  ___    _ ___ ___ _____ ___ _ 
   / _ \| _ )_ | | __/ __|_   _/ __| |
  | (_) | _ \ || | _| (__  | | \__ \_|
   \___/|___/\__/|___\___| |_| |___(_)
-                                     
-    
-    */
-
-
-// Card Arrays
-// var lightArr = [{
-//     cardName: "",
-//     Health: "",
-//     atk: ""
-
-// }]; // add card bg later
-// var darkArr = [{
-//     cardName: "",
-//     Health: "",
-//     atk: ""
-// }];
-
-/*
-
-  _     ___ ____ _   _ _____ 
- | |   |_ _/ ___| | | |_   _|
- | |    | | |  _| |_| | | |  
- | |___ | | |_| |  _  | | |  
- |_____|___\____|_| |_| |_|  
-                                                                                                 
-    */
-
-var lightCommander = {
-    name: "Light Commander",
-    Health: 32,
-    atk: 18,
-};
-
-var lightArcher = {
-    name: "Light Archer",
-    Health: 17,
-    atk: 8
-};
-
-var lightSoldier = {
-    name: "Light Soldier",
-    Health: 17,
-    atk: 8
-};
-
-var lightDog = {
-    name: "Light Dawg",
-    Health: 15,
-    atk: 10000000 // Richard what the fuck? XD
-}
-
-/*
-  ____    _    ____  _  __
- |  _ \  / \  |  _ \| |/ /
- | | | |/ _ \ | |_) | ' / 
- | |_| / ___ \|  _ <| . \ 
- |____/_/   \_\_| \_\_|\_\
-                          
-*/
-var darkCommander = {
-    name: "Dark Commander",
-    Health: 26,
-    atk: 24
-};
-
-var darkWizard = {
-    name: "Dark Wizard",
-    Health: 15,
-    atk: 10
-};
-
-var darkGrunt = {
-    name: "Drunk Grunt",
-    atk: 5,
-    Health: 20
-}
-var darkDog = {
-    name: "Dark Dog",
-    atk: 7,
-    Health: 18
-}
-
-// new henry obj
+  */
+    // new henry obj
 var gameobj = {
     lghtSide: {
         "lightCommander": {
             name: "Light Commander",
             health: 32,
-            atk: 18
+            atk: 18,
+            type: "light"
         },
         "lightDog": {
             name: "Light Dog",
             health: 15,
-            atk: 10
+            atk: 10,
+            type: "light"
         },
         "lightSoldier": {
             name: "Light Soldier",
             health: 17,
-            atk: 8
+            atk: 8,
+            type: "light"
         },
         "lightArcher": {
             name: "Light Archer",
             health: 15,
-            atk: 10
+            atk: 10,
+            type: "light"
         }
     },
     drkSide:{
         "darkCommander": {
             name: "Dark Commander",
             health: 26,
-            atk: 24
+            atk: 24,
+            type: "dark"
         },
         "darkDog":{
-            name:"Dark Dog",
+            name: "Dark Dog",
             health:18,
-            atk:7
+            atk:7,
+            type: "dark"
         },
         "darkGrunt":{
             name: "Dark Grunt",
             health: 20,
-            atk: 5
+            atk: 5,
+            type: "dark"
         },
         "darkWizard":{
             name: "Dark Wizard",
             health: 15,
-            atk: 10
+            atk: 10,
+            type: "dark"
         }
     },
 };
@@ -199,106 +123,45 @@ var cardObj = {
 // }
 
 
+// How to make loop for clicks?????
 
-function changeDiv(id){
-    var name = gameobj.drkSide[id].name;
-    var atk = gameobj.drkSide[id].atk;
-    var health = gameobj.drkSide[id].health;
+function changeDiv(id, type){
+
+    var mon = null;
     
-    cName.innerHTML = drkSide[id].name;
-    cHp.innerHTML = "Health: " + drkSide[id].health;
-    cAtk.innerHTML = "ATK: " + drkSide[id].atk;
+    if(type == "dark"){
+        mon = gameobj.drkSide[id];
+    } else if (type == "light"){
+        mon = gameobj.lghtSide[id];
+    }
+    
+    cName.innerHTML = mon.name;
+    cHp.innerHTML = "Health: " + mon.health;
+    cAtk.innerHTML = "Attack: " + mon.atk;
+    hoverPanel.style.display = "block";
 
-    cName.innerHTML = lghtSide[id].name;
-    cHp.innerHTML = "Health: " + lghtSide[id].health;
-    cAtk.innerHTML = "ATK: " + lghtSide[id].atk;
+    /*cName.innerHTML = gameobj.lghtSide[id].name;
+    cHp.innerHTML = "Health: " + gameobj.lghtSide[id].health;
+    cAtk.innerHTML = "ATK: " + gameobj.lghtSide[id].atk;*/
     //innerHTML stuff
 }
 
-var cards = document.getElementsByClassName("mon");
+monStats = document.getElementsByClassName("allMins");
 
-// How to loop
-for(var i =0;i<cards.length; i++){
-    cards[i].addEventListener("click", function(){
-        changeDiv(this.id);
+// Light Loop
+for(var i =0;i<monStats.length; i++){
+    monStats[i].addEventListener("click", function(){
+        changeDiv(this.id, "light");
+        console.log("Minion Clicked");
     })
 }
-
-
-LCommander.addEventListener("click", function () {   
-    hoverPanel.style.display = "block";
-    console.log(LCommander);
-    cName.innerHTML = lightCommander.name
-    cHp.innerHTML = "Health: " + lightCommander.Health;
-    cAtk.innerHTML = "ATK: " + lightCommander.atk;
-});
-// Light Archer Click Function
-LArcher.addEventListener("click", function () {
-    hoverPanel.style.display = "block";
-    console.log(LArcher);
-    cName.innerHTML = lightArcher.name;
-    cHp.innerHTML = "Health: " + lightArcher.Health;
-    cAtk.innerHTML = "ATK: " + lightArcher.atk;
-});
-
-// Light Soldier Click Function
-LSoldier.addEventListener("click", function () {
-    hoverPanel.style.display = "block";
-    console.log(LSoldier);
-    cName.innerHTML = lightSoldier.name
-    cHp.innerHTML = "Health: " + lightSoldier.Health;
-    cAtk.innerHTML = "ATK: " + lightSoldier.atk;
-})
-
-// Light Dog Click Function
-LDog.addEventListener("click", function () {
-    hoverPanel.style.display = "block";
-    console.log(LDog);
-    cName.innerHTML = lightDog.name
-    cHp.innerHTML = "Health: " + lightDog.Health;
-    cAtk.innerHTML = "ATK: " + lightDog.atk;
-})
-
-// Dark Champion Click Function
-DCommander.addEventListener("click", function () {
-    hoverPanel.style.display = "block";
-    console.log(darkCommander);
-    cName.innerHTML = darkCommander.name;
-    cHp.innerHTML = "Health: " + darkCommander.Health;
-    cAtk.innerHTML = "ATK: " + darkCommander.atk;
-});
-
-// Dark Wizard Click Function
-DWizard.addEventListener("click", function () {
-    hoverPanel.style.display = "block";
-    console.log(DWizard);
-    // darkMin[0].name;
-    // cName.innerHTML = darkMin[0].name;
-
-    cName.innerHTML = darkWizard.name;
-    cHp.innerHTML = "Health: " + darkWizard.Health;
-    cAtk.innerHTML = "ATK: " + darkWizard.atk;
-    
-});
-
-// Dark Grunt Click Function
-DGrunt.addEventListener("click", function () {
-    hoverPanel.style.display = "block";
-    console.log(DGrunt);
-    cName.innerHTML = darkGrunt.name;
-    cHp.innerHTML = "Health: " + darkGrunt.Health;
-    cAtk.innerHTML = "ATK: " + darkGrunt.atk;
-});
-
-// Dark Dog Click Function  
-DDog.addEventListener("click", function () {
-    hoverPanel.style.display = "block";
-    console.log(DDog);
-    cName.innerHTML = darkDog.name;
-    cHp.innerHTML = "Health: " + darkDog.Health;
-    cAtk.innerHTML = "ATK: " + darkDog.atk;
-    console.log("Get clicked!!!");
-});
+// Dark Loop
+for(var i =0;i<monStats.length; i++){
+    monStats[i].addEventListener("click", function(){
+        changeDiv(this.id, "dark");
+        console.log("Minion Clicked");
+    })
+}
 
 /*
         
@@ -325,36 +188,11 @@ function hpCalc() {
     this.cardObj.Health + darkMin[i].atk;
 }
 
-/*
-var lightCommander = {
-    name: "Light Commander",
-    Health: 32,
-    atk: 18,
-};
-
-var lightArcher = {
-    name: "Light Archer",
-    Health: 17,
-    atk: 8
-};
-
-var lightSoldier = {
-    name: "Light Soldier",
-    Health: 17,
-    atk: 8
-};
-
-var lightDog = {
-    name: "Light Dawg",
-    Health: 15,
-    atk: 10000000 // Richard what the fuck? XD
-}*/
-
-
 // jQuery tears
 
 
 //when cards are added
+
 /*
 gameobj.drkSide[newid] = {
 
